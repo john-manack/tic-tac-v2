ea1922a4926cb2632e11ed6882ad2a292799b0cd
John Manack <jmnck28@gmail.com>
2021-02-25 09:08:04 -0500

    establishes basic file structure with component jsx files

---
+++ src/components/Square.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Square extends React.Component {
+    render() {
+        return (
+            <button className="square">
+                {/* TO DO */}
+            </button>
+        );
+    }
+}
+
+export default Square;
